fb0f4935c2dc2d84fe1738fa7dea401862c33462
emredersiniz <112582013+emre-cavus@users.noreply.github.com>
2022-12-30 20:48:49 +0300

    Add files via upload

---
+++ Renkli butonlar/Renkli butonlar/Form2.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Renkli_butonlar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SlateBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(247, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 146);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PASTEL BLUE MODE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(247, 319);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 155);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "PASTEL GREEN MODE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Salmon;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(458, 125);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 146);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "PASTEL RED MODE";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Khaki;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(458, 319);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(161, 146);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "PASTEL YELLOW MODE";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 610);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "RENgaRENK";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Sınav_Çalışmaları vol.3/Sınav_Çalışmaları vol.3/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sınav_Çalışmaları_vol._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte ortalama;
+            ortalama = Convert.ToByte(textBox1.Text);
+            if (ortalama >= 50)
+            {
+                if (ortalama >= 85)
+                {
+                    label2.Text = "Takdir Belgesi Almaya Hak Kazandınız.";
+                }
+                else if (ortalama >= 70)
+                {
+                    label2.Text = "Teşekkür Belgesi Almaya Hak Kazandınız.";
+                }
+                else
+                { label2.Text = "Belge Almadan Sınıf Geçtiniz."; }
+            }
+            else
+            { label2.Text = "Sınıf Geçmek İçin Yeterli Not Alamadınız."; }
+        }
+    }
+}
